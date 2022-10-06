96b1e794b9aa25b7b23583e0ddfc50dda8879723
LucasArrabal <101684353+LucasArrabal@users.noreply.github.com>
2022-10-06 14:31:44 -0300

    Update Solucao.cs

--- Aumento de Salario/Solucao.cs
+++ Aumento de Salario/Solucao.cs
@@ -1,51 +1,60 @@
 using System;
 
-class CorridaDeTartaturgas 
+class AumentoDeSalario
 {
-      public static void Main() 
-      {
-        
-      int quantidadeEntradas = 3;
-      
-      while(quantidadeEntradas > 0)
-      {
-        var numeroQuantidade = Int32.Parse(Console.ReadLine());
-        
-        if(numeroQuantidade >= 1 && numeroQuantidade <= 500)
+    static void Main(string[] args)
+    {
+        string salarioStr = "";
+        double salario= 0.00;
+        double novoSalario = 0.00;
+        double reajuste = 0.00;
+        double percentual = 0.00;
+
+        salarioStr = Console.ReadLine().Replace(",",".");
+        salario = double.Parse(salarioStr);
+    
+        if (salario < 0.00) 
         {
-          
-          string[] tartarugas = Console.ReadLine().Split(" ");
-          
-          var maiorVelocidade = Int32.Parse(tartarugas[0]);
-          
-          for(int i = 1; i < numeroQuantidade; i++)
-          {
-            var tartaruga = Int32.Parse(tartarugas[i]);
-            
-            if( tartaruga > maiorVelocidade)
-            {
-              maiorVelocidade = tartaruga;
-            }
-          }
-          
-          if(maiorVelocidade < 10)
-          {
-            Console.WriteLine(1);
-          }
-          else if(maiorVelocidade >= 10 && maiorVelocidade < 20)
-          {
-            Console.WriteLine(2);
-          }
-          else if(maiorVelocidade >= 20)
-          {
-            Console.WriteLine(3);
-          }
-          quantidadeEntradas--;
-        } 
+            return;
+        }
+
+        else if (salario <= 400.00)
+        {
+            percentual = 0.15;
+            reajuste = salario * percentual;
+            novoSalario = salario + reajuste;
+        }
+
+        else if (salario <= 800.00) 
+        {
+            percentual = 0.12;
+            reajuste = salario * percentual;
+            novoSalario = salario + reajuste;
+        }
+
+        else if (salario <= 1200.00)
+        {
+            percentual = 0.10;
+            reajuste = salario * percentual;
+            novoSalario = salario + reajuste;
+        }
+
+        else if (salario <= 2000.00)
+        {
+            percentual = 0.07;
+            reajuste = salario * percentual;
+            novoSalario = salario + reajuste;
+        }
+
         else 
         {
-          Console.WriteLine("Insira um número entre 1 e 500");
+            percentual = 0.04;
+            reajuste = salario * percentual;
+            novoSalario = salario + reajuste;
         }
-      }
+    
+        Console.WriteLine("Novo salario: {0:0.00}", novoSalario);
+        Console.WriteLine("Reajuste ganho: {0:0.00}", reajuste);
+        Console.WriteLine("Em percentual: {0} %", percentual * 100);
     }
 }
